cbeff49694e676c86299fc9950879aa63565b2ae
Nguyen Hoang Huy <huynh@vacsvnhcm.local>
2024-12-04 17:32:39 +0700

    first commit

---
+++ src/modules/header/HeaderView.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <p class="text-red-500">Header</p>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HomeView from '@/modules/header/HeaderView.vue'
+</script>
+
+<template>
+  <main>
+    <HomeView />
+  </main>
+</template>
